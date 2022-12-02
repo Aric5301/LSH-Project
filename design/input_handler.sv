d69ecfd1beacfdf52dbd2cee078f72bec8b85692
Elad Ben Shlush Aric Radzin <epebar@vlsi-ria60.technion.ac.il>
2022-12-02 19:31:57 +0200

    window_hasher is working!

--- design/input_handler.sv
+++ design/input_handler.sv
@@ -20,38 +20,38 @@ module input_handler #(
 int fd, i = 1;
 reg[1000:1] str;
 string line[128];
+function string split_using_delimiter_fn(input int offset, string str,string del,output int cnt);
+	for (int i = offset; i < str.len(); i= i+1) 
+	  if (str.getc(i) == del) begin
+		 cnt = i;
+		 return str.substr(offset,i-1);
+	   end
+  endfunction
 
 initial begin
-	fd = $fopen("reference", "r");
 	
-for (i = 0 ;; i++) begin
+	fd = $fopen("reference_sv", "r");
+	
+
 	window_reset = 1;
 	ready_for_hashing = 0;
-	$fseek(fd, (WINDOW_SIZE - (KMER_SIZE - 1)) * i, 0);
-	$fgets(line[i], fd);
-
-	foreach (line[j]) begin
-		$display("%s", line[j]);
-		case(line[j])
-			"A" : window[j] = 2'b00;
-			"C" : window[j] = 2'b01;
-			"G" : window[j] = 2'b10;
-			"T" : window[j] = 2'b11;
-		endcase
-		window_id = i;
-		window_reset = 0;
-		#1
-		ready_for_hashing = 1;
-		while (!(hashing_is_done)) begin
-		#1;
-		end
-		#2;
-		is_insert = 1;
-		end
-
+	$fgets(line, fd);
+	//foreach (line[j]) begin
+	for(int j =0 ; j < WINDOW_SIZE ; j = j++) begin
+		window[j] = split_using_delimiter_fn(p_offset_in, line, "['", p_offset_out);
+		window[j] = split_using_delimiter_fn(p_offset_in, line, ", ", p_offset_out);
 	end
-  
-		
+	window_id = i;
+	window_reset = 0;
+	#1
+	ready_for_hashing = 1;
+	while (!(hashing_is_done)) begin
+		#1;
+	end
+	#2;
+	is_insert = 1;
+
+	
 	$fclose(fd);
 end
 endmodule
--- design/window_hasher.sv
+++ design/window_hasher.sv
@@ -8,10 +8,10 @@
 
 module window_hasher #(
 	parameter SKETCH_SIZE         =16,
-	          NUM_OF_BUCKETS      =256,
-	          LOG2_NUM_OF_BUCKETS =8,
-	          WINDOW_SIZE         =128,
-	          KMER_SIZE           =16
+			  NUM_OF_BUCKETS      =256,
+			  LOG2_NUM_OF_BUCKETS =8,
+			  WINDOW_SIZE         =128,
+			  KMER_SIZE           =16
 ) (
 	
 	input	logic                        clk,
@@ -24,14 +24,14 @@ module window_hasher #(
 	output logic                           hashing_is_done                 // turns on for one clock cycle when ready
 );
 
-logic [31:0] h1_all_kmers  [0:WINDOW_SIZE-KMER_SIZE+1]; // h1 run on all possible kmers
-logic [LOG2_NUM_OF_BUCKETS-1:0] h2_all_kmers  [0:WINDOW_SIZE-KMER_SIZE+1]; // h2 run on all possible kmers
+logic [31:0] h1_all_kmers  [0:WINDOW_SIZE - KMER_SIZE]; // h1 run on all possible kmers
+logic [LOG2_NUM_OF_BUCKETS-1:0] h2_all_kmers  [0:WINDOW_SIZE - KMER_SIZE]; // h2 run on all possible kmers
 
 logic is_hashing_active;
 
 genvar i;
 generate
-	for (i = 0 ; i <= WINDOW_SIZE - 1 - KMER_SIZE + 1 ; i++) begin
+	for (i = 0 ; i <= WINDOW_SIZE - KMER_SIZE; i++) begin
 		hasher u0 (
 			.kmer(window[i:i + KMER_SIZE - 1]),
 			.h1  (h1_all_kmers[i]            ),
@@ -56,53 +56,48 @@ always @(posedge clk or posedge reset) begin
 		index_in_h1_all_kmers = 0;
 		is_hashing_active = 0;
 		hashing_is_done = 0;
+		hashedSketch = '{default: '0};
 	end
 	
 	else if (ready_for_hashing == 1 && is_hashing_active == 0) begin
 		
 		is_hashing_active = 1;
+		// dynamic_h1_all_kmers[0:WINDOW_SIZE - KMER_SIZE] = h1_all_kmers[0:WINDOW_SIZE - KMER_SIZE];
 	end
 	
 	else if (is_hashing_active == 1 && hashing_is_done == 0) begin
 		
 		if (index_in_h1_all_kmers == 0) begin
 			
-			if (h1_all_kmers[index_in_h1_all_kmers] >= currentSmallest) begin
+			h1_sketch[index_in_h1_sketch] = -1;
+		end
+		
+		if (h1_all_kmers[index_in_h1_all_kmers] > currentSmallest
+				&& h1_all_kmers[index_in_h1_all_kmers] <= h1_sketch[index_in_h1_sketch]) begin
+			
+			h1_sketch[index_in_h1_sketch] = h1_all_kmers[index_in_h1_all_kmers];
+			hashedSketch[index_in_h1_sketch] = h2_all_kmers[index_in_h1_all_kmers];
+		end
+		
+		if (index_in_h1_all_kmers == WINDOW_SIZE - KMER_SIZE) begin
+			
+			if (index_in_h1_sketch == SKETCH_SIZE - 1) begin
 				
-				h1_sketch[index_in_h1_sketch] = h1_all_kmers[index_in_h1_all_kmers];
-				hashedSketch[index_in_h1_sketch] = h2_all_kmers[index_in_h1_all_kmers];
+				hashing_is_done = 1;
 			end
-			index_in_h1_all_kmers += 1;
+			
+			else begin
+			
+				currentSmallest = h1_sketch[index_in_h1_sketch];
+				index_in_h1_sketch += 1;
+				index_in_h1_all_kmers = 0;
+				
+			end
 		end
 		
 		else begin
 			
-			if (h1_all_kmers[index_in_h1_all_kmers] >= currentSmallest
-					&& h1_all_kmers[index_in_h1_all_kmers] < h1_sketch[index_in_h1_sketch]) begin
-				
-				h1_sketch[index_in_h1_sketch] = h1_all_kmers[index_in_h1_all_kmers];
-				hashedSketch[index_in_h1_sketch] = h2_all_kmers[index_in_h1_all_kmers];
-			end
-			
-			if (index_in_h1_all_kmers == WINDOW_SIZE-KMER_SIZE+1) begin
-				
-				if (index_in_h1_sketch == SKETCH_SIZE - 1) begin
-					
-					hashing_is_done = 1;
-				end
-				
-				else begin
-					
-					currentSmallest = h1_sketch[index_in_h1_sketch];
-					index_in_h1_sketch += 1;
-					index_in_h1_all_kmers = 0;
-				end
-			end
-			
-			else begin
-				
-				index_in_h1_all_kmers += 1;
-			end
+			index_in_h1_all_kmers += 1;
 		end
 	end
 end
